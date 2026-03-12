Update: Fix the variance. The clip was not supposed to be there

loss/FMLoss.py
@@ -93,7 +93,7 @@ class STDIMLoss(FMLoss):
         super(STDIMLoss, self).__init__()
 
         self.model = model
-        self.projection1 = torch.nn.Linear(feature_size, local_layer_depth).to(device)  # x1 = global, x2=patch, n_channels = 32
+        self.projection1 = torch.nn.Linear(feature_size, local_layer_depth).to(device)
         self.projection2 = torch.nn.Linear(local_layer_depth, local_layer_depth).to(device)
         self.device = device
 